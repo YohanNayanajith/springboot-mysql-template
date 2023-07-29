97d79627ce74a41b3c9965061113116a39bc3f57
YohanNayanajith <yohannayanajith40@gmail.com>
2023-07-29 15:51:26 +0530

    first commit

---
+++ src/main/java/com/example/mysqltemplate/MysqlTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqltemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mysqltemplate/MysqlTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqltemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
